59eb4b85f51e213544e561dd17d678bf1b60ddd4
Bogdan Tarynskyi <tarynskyi.bohdan@gmail.com>
2025-05-21 17:54:51 +0300

    Add GitHub Actions workflow for Django tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Recipe
+
+class RecipeViewsTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Desserts")
+        self.recipe = Recipe.objects.create(
+            title="Chocolate Cake",
+            description="Delicious and rich",
+            instructions="Mix and bake",
+            ingredients="flour, sugar, cocoa",
+            category=self.category,
+        )
+
+    def test_main_view_status_code(self):
+        url = reverse('main')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Recipes")
+        self.assertContains(response, self.recipe.title)
+
+    def test_category_detail_view_status_code(self):
+        url = reverse('category_detail', args=[self.category.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.recipe.title)
+
+    def test_category_detail_view_empty(self):
+        empty_category = Category.objects.create(name="Empty Category")
+        url = reverse('category_detail', args=[empty_category.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No recipes found.")
+
